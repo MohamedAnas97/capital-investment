f0ce6b8586a2890201c6a85bdc32e855d13a1b50
MohamedAnas97 <mohamed.a@gnginfotech.com>
2024-06-03 11:19:03 +0400

    first commit

--- src/components/risk-management/banner.tsx
+++ src/components/risk-management/banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div>
       <div className="w-full">
         <Image
-          src={require("/public/assets/risk-management/banner.png")}
+          src={require("/public/assets/risk-management/Banner.png")}
           alt=""
           className=" w-full lg:object-cover"
         />
@@ -16,11 +16,11 @@ const Banner = () => {
         </h4>
         <p className="text-[16px] text-[#606060] font-[400] leading-[24px] font-inter pt-3">
           At Anza Capital Investment, we recognize that successful investing
-          isn&apos;t just about chasing high returns; it&apos;s about safeguarding your
-          investments against potential risks. With our robust risk management
-          strategies, we&apos;re dedicated to helping our clients navigate the
-          complexities of the financial markets with confidence and peace of
-          mind.
+          isn&apos;t just about chasing high returns; it&apos;s about
+          safeguarding your investments against potential risks. With our robust
+          risk management strategies, we&apos;re dedicated to helping our
+          clients navigate the complexities of the financial markets with
+          confidence and peace of mind.
         </p>
       </div>
       <div className="mx-[5%] xl:mx-[7%] py-5">
